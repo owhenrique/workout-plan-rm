efd96a6736e0592a9c564d47745257ee348a97be
owhenrique <me.pauloalmeida@gmail.com>
2024-03-31 20:45:48 -0300

    refactor: add trainer cardio to workouts

--- src/mocks/treinoA.ts
+++ src/mocks/treinoA.ts
@@ -11,6 +11,7 @@ const treinoA: Treino = {
       { imagem: "https://www.blog.treinoemalta.com.br/wp-content/uploads/2023/07/Rosca-Alternada-com-Halteres.gif", nome: "Rosca alternada c/ halteres", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2023/04/dumbbell-hammer-curl-v-2.gif", nome: "Rosca martelo c/ halteres", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/11/Rosca-Scott-com-halteres-unilateral.gif", nome: "Rosca Scott unilateral", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/04/corrida-na-esteira-inclinada-1.gif", nome: "Esteira", duracao: 15 }
     ]
 };
   
--- src/mocks/treinoB.ts
+++ src/mocks/treinoB.ts
@@ -8,6 +8,7 @@ const treinoB: Treino = {
       { imagem: "https://media.tenor.com/ZElx6PviDq0AAAAM/gym.gif", nome: "Cadeira flexora", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.hipertrofia.org/blog/wp-content/uploads/2017/11/smith-full-squat.gif", nome: "Agachamento Smith", series: 3, repeticoes: 12, descanso: 60 },
       { imagem: "https://www.blog.treinoemalta.com.br/wp-content/uploads/2023/07/sentado.gif", nome: "Panturilha sentado", series: 3, repeticoes: 12, descanso: 60 },
+      { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/04/corrida-na-esteira-inclinada-1.gif", nome: "Esteira", duracao: 15 }
     ]
   };
   
